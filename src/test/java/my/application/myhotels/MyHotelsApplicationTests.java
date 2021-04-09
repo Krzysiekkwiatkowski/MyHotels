755bbd59d3024d915941b35947c6c51cc3f00426
Krzysiekkwiatkowski <krzyszt.kwiatkowski@gmail.com>
2021-04-10 01:15:18 +0200

    Initial commit

---
+++ src/test/java/my/application/myhotels/MyHotelsApplicationTests.java
@@ -0,0 +1,13 @@
+package my.application.myhotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHotelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
